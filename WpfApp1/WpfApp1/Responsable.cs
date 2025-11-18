6e15471f2515d518fa946393d95bea742ece7a4d
Victor <carusvictor66@gmail.com>
2025-11-18 09:45:01 +0100

    Ambas clases añadidas en el proyecto principal

---
+++ WpfApp1/WpfApp1/Responsable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Responsable
+    {
+        private int codigo;
+        private string nombre;
+        private string apellidos;
+
+        public Responsable(int codigo, string nombre, string apellidos)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+
+        public int ObtenerCodigo() => codigo;
+        public string ObtenerNombre() => nombre;
+        public string ObtenerApellidos() => apellidos;
+
+        public void CambiarNombre(string nuevoNombre) => nombre = nuevoNombre;
+        public void CambiarApellidos(string nuevosApellidos) => apellidos = nuevosApellidos;
+    }
+
+}
